e7df25033617c10b2923a41eaa2f986883700c40
Sodiqov <96613827+Sodiqovj@users.noreply.github.com>
2022-06-16 10:17:26 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let iframe = document.querySelector('iframe')
+let input = document.querySelector('.link')
+let btn = document.querySelector('.btn')
+
+btn.addEventListener('click',function(){
+    var link = input.value
+    btn.style.display = 'none'
+    download(link)
+})
+function download(link){
+    iframe.src=`https://loader.to/api/button/?url=${link}&f=360`
+}
