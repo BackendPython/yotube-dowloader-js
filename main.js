9333db7fe6306cd9cb2f37e23bdd69b04053122a
Sodiqov <96613827+Sodiqovj@users.noreply.github.com>
2022-06-16 11:08:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 let iframe = document.querySelector('iframe')
+let select = document.querySelector('.format')
 let input = document.querySelector('.link')
 let btn = document.querySelector('.btn')
 
 btn.addEventListener('click',function(){
     if(input.value!=''){
         var link = input.value
+        var format = select.children('option:selected').value
         btn.style.display = 'none'
-        download(link) 
+        download(link, format) 
     }
 })
-function download(link){
-    iframe.src=`https://loader.to/api/button/?url=${link}&f=360`
+function download(link, format){
+    iframe.src=`https://loader.to/api/button/?url=${link}&f=${format}`
 }
