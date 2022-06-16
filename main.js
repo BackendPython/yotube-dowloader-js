337e427c8e57379320bbb733e9aa12c25069c703
Sodiqov <96613827+Sodiqovj@users.noreply.github.com>
2022-06-16 10:26:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ let input = document.querySelector('.link')
 let btn = document.querySelector('.btn')
 
 btn.addEventListener('click',function(){
-    var link = input.value
-    btn.style.display = 'none'
-    download(link)
+    if(input.value!=''){
+        var link = input.value
+        btn.style.display = 'none'
+        download(link) 
+    }
 })
 function download(link){
     iframe.src=`https://loader.to/api/button/?url=${link}&f=360`
