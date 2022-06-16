e219df7e9da17d0edab04fe8c50700de8f9aee50
Sodiqov <96613827+Sodiqovj@users.noreply.github.com>
2022-06-16 11:27:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ let btn = document.querySelector('.btn')
 btn.addEventListener('click',function get(){
     if(input.value!=''){
         var link = input.value
-        var as = document.forms[0].ddlViewBy.value;
         var format = select.options[select.selectedIndex].value;
         btn.style.display = 'none'
         download(link, format)
